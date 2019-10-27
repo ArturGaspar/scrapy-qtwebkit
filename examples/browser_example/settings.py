0329813c200e5f1f1942bfe0bebfcf590c22c798
Artur Gaspar <artur.gaspar.00@gmail.com>
2019-10-27 10:07:42 -0300

    Clean up examples.

---
+++ examples/browser_example/settings.py
@@ -0,0 +1,24 @@
+# Scrapy settings for browser_example project
+
+SPIDER_MODULES = ['browser_example.spiders']
+NEWSPIDER_MODULE = 'browser_example.spiders'
+
+ROBOTSTXT_OBEY = False
+
+DOWNLOADER_MIDDLEWARES = {
+    'scrapy_qtwebkit.middleware.BrowserMiddleware': 200
+}
+COOKIES_ENABLED = False
+BROWSER_ENGINE_COOKIES_ENABLED = True
+
+BROWSER_ENGINE_SERVER = 'tcp:localhost:8000'
+# BROWSER_ENGINE_START_SERVER = True
+
+# Enable and configure HTTP caching (disabled by default)
+# See https://doc.scrapy.org/en/latest/topics/downloader-middleware.html#httpcache-middleware-settings
+#HTTPCACHE_ENABLED = True
+#HTTPCACHE_EXPIRATION_SECS = 0
+#HTTPCACHE_DIR = 'httpcache'
+#HTTPCACHE_IGNORE_HTTP_CODES = []
+#HTTPCACHE_STORAGE = 'scrapy.extensions.httpcache.FilesystemCacheStorage'
+
--- examples/browser_example/spiders/example.py
+++ examples/browser_example/spiders/example.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 from scrapy import Spider
 from twisted.internet.defer import inlineCallbacks
 
